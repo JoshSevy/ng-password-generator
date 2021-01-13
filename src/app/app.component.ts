52bf6848edffe70a75a9bd2f9e7677950be01c2f
JoshSevy <joshuasevy@outlook.com>
2021-01-13 14:40:22 -0700

    Add logic to generate a random password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,24 +6,56 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  includeLetters = false;
-  includeNumbers = false;
-  includeSymbols = false;
-  password = '';
+  length: number = 0;
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
+  password: string = '';
 
-  onChangeUseLetters() {
+  onChangeLength(value: string): void {
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onChangeUseLetters(): void {
     this.includeLetters = !this.includeLetters;
   }
 
-  onChangeUseNumbers() {
+  onChangeUseNumbers(): void {
     this.includeNumbers = !this.includeNumbers;
   }
 
-  onChangeUseSympbols() {
+  onChangeUseSymbols(): void {
     this.includeSymbols = !this.includeSymbols;
   }
 
-  onButtonClick() {
-    this.password = 'MY PASSWORD!!!';
+  onButtonClick(): void {
+    console.log(`
+      About to generate a password with the following:
+      Includes Letters: ${this.includeLetters}
+      Includes Numbers: ${this.includeNumbers}
+      Includes Symbols: ${this.includeSymbols}
+    `)
+
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if (this.includeLetters) validChars += letters;
+    if (this.includeNumbers) validChars += numbers;
+    if (this.includeSymbols) validChars += symbols;
+
+    let generatedPassword = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+    this.password = generatedPassword;
   }
 }
